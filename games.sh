d71dedbdfc95e66a62103a732fb76d169739d1f6
Pawel Grabinski <pawelrgrabinski@gmail.com>
2024-03-20 11:32:02 +0000

    Update training command to include k_hop=3

--- games.sh
+++ games.sh
@@ -1,2 +1,2 @@
 #!/user/bin/env/ bash
-python -u  new_main.py --data=Games --gpu=3 --epoch=20 --hidden_size=50 --batchSize=50 --user_long=orgat --user_short=att --item_long=orgat --item_short=att --user_update=rnn --item_update=rnn --lr=0.001 --l2=0.0001 --layer_num=2 --item_max_length=50 --user_max_length=50 --attn_drop=0.3 --feat_drop=0.3 --record >./jup&
+python -u  new_main.py --data=Games --gpu=1 --epoch=2 --hidden_size=10 --batchSize=50 --user_long=orgat --user_short=att --item_long=orgat --item_short=att --user_update=rnn --item_update=rnn --lr=0.001 --l2=0.0001 --layer_num=2 --item_max_length=50 --user_max_length=50 --attn_drop=0.3 --feat_drop=0.3 --record --k_hop=3>./jup&
